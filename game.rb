c311422bba2997786b261e5a4011859e7949e272
palomaviolin <paloma.aniabenito@gmail.com>
2021-02-02 22:30:01 +0100

    Step 3

--- game.rb
+++ game.rb
@@ -4,7 +4,13 @@
 puts "Welcome to 'Get My Numner!'"
 print "What's your name?"
 
+# Get the player's name, and greet them.
+print "What's your name?"
 input = gets
 name = input.chomp
-
 puts "Welcome, #{name}!"
+
+# Store a random number for the player to guess.
+puts "I've got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1
